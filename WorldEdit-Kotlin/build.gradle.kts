395c83ed132d0a7fbc10bf941d1a86e564661699
MLGPenguin <mlgpenguboi@gmail.com>
2023-08-01 09:56:23 +1000

    Add maven publishing

--- WorldEdit-Kotlin/build.gradle.kts
+++ WorldEdit-Kotlin/build.gradle.kts
@@ -4,3 +4,15 @@ plugins {
 
 group = "me.superpenguin.libraries"
 version = "1.0.0"
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "me.superpenguin.superglue"
+            artifactId = "worldeditwrapper-kotlin"
+            version = "1.0.0"
+
+            from(components["kotlin"])
+        }
+    }
+}
